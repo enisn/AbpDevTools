87145f6c5402937907dadd5a7adfe9ad46320d15
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-04-27 11:08:07 +0300

    Update BuildCommand.cs

--- Commands/BuildCommand.cs
+++ Commands/BuildCommand.cs
@@ -8,96 +8,96 @@ namespace AbpDevTools.Commands;
 [Command("build", Description = "Shortcut for dotnet build /graphBuild")]
 public class BuildCommand : ICommand
 {
-	[CommandParameter(0, IsRequired = false, Description = "Working directory to run build. Probably project or solution directory path goes here. Default: . (Current Directory)")]
-	public string WorkingDirectory { get; set; }
+    [CommandParameter(0, IsRequired = false, Description = "Working directory to run build. Probably project or solution directory path goes here. Default: . (Current Directory)")]
+    public string WorkingDirectory { get; set; }
 
-	Process runningProcess;
+    Process runningProcess;
 
-	public async ValueTask ExecuteAsync(IConsole console)
-	{
-		if (string.IsNullOrEmpty(WorkingDirectory))
-		{
-			WorkingDirectory = Directory.GetCurrentDirectory();
-		}
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        if (string.IsNullOrEmpty(WorkingDirectory))
+        {
+            WorkingDirectory = Directory.GetCurrentDirectory();
+        }
 
-		var cancellationToken = console.RegisterCancellationHandler();
+        var cancellationToken = console.RegisterCancellationHandler();
 
-		var buildFiles = await AnsiConsole.Status()
-		   .StartAsync("Looking for solution files (.sln)", async ctx =>
-		   {
-			   ctx.Spinner(Spinner.Known.SimpleDotsScrolling);
-			   var slns = Directory.EnumerateFiles(WorkingDirectory, "*.sln", SearchOption.AllDirectories)
-				   .Select(x => new FileInfo(x))
-				   .ToArray();
+        var buildFiles = await AnsiConsole.Status()
+           .StartAsync("Looking for solution files (.sln)", async ctx =>
+           {
+               ctx.Spinner(Spinner.Known.SimpleDotsScrolling);
+               var slns = Directory.EnumerateFiles(WorkingDirectory, "*.sln", SearchOption.AllDirectories)
+                   .Select(x => new FileInfo(x))
+                   .ToArray();
 
-			   AnsiConsole.MarkupLine($"[green]{slns.Length}[/] .sln files found.");
+               AnsiConsole.MarkupLine($"[green]{slns.Length}[/] .sln files found.");
 
-			   return slns;
-		   });
+               return slns;
+           });
 
-		if (buildFiles.Length == 0)
-		{
-			await console.Output.WriteLineAsync("No .sln files found. Looking for .csproj files.");
+        if (buildFiles.Length == 0)
+        {
+            await console.Output.WriteLineAsync("No .sln files found. Looking for .csproj files.");
 
-			buildFiles = await AnsiConsole.Status()
-				.StartAsync("Looging for C# Projects (.csproj)", async ctx =>
-				{
-					ctx.Spinner(Spinner.Known.SimpleDotsScrolling);
-					var csprojs = Directory.EnumerateFiles(WorkingDirectory, "*.csproj", SearchOption.AllDirectories)
-						.Select(x => new FileInfo(x))
-						.ToArray();
+            buildFiles = await AnsiConsole.Status()
+                .StartAsync("Looging for C# Projects (.csproj)", async ctx =>
+                {
+                    ctx.Spinner(Spinner.Known.SimpleDotsScrolling);
+                    var csprojs = Directory.EnumerateFiles(WorkingDirectory, "*.csproj", SearchOption.AllDirectories)
+                        .Select(x => new FileInfo(x))
+                        .ToArray();
 
-					AnsiConsole.MarkupLine($"[green]{csprojs.Length}[/] .csproj files found.");
-					return csprojs;
-				});
-		}
+                    AnsiConsole.MarkupLine($"[green]{csprojs.Length}[/] .csproj files found.");
+                    return csprojs;
+                });
+        }
 
-		await AnsiConsole.Status().StartAsync("Starting build...", async ctx =>
-		{
-			for (int i = 0; i < buildFiles.Length; i++)
-			{
-				var buildFile = buildFiles[i];
-				var progressRatio = $"[yellow]{i + 1}/{buildFiles.Length}[/]";
-				ctx.Status($"{progressRatio} - [bold]Building[/] {buildFile.FullName}");
+        await AnsiConsole.Status().StartAsync("Starting build...", async ctx =>
+        {
+            for (int i = 0; i < buildFiles.Length; i++)
+            {
+                var buildFile = buildFiles[i];
+                var progressRatio = $"[yellow]{i + 1}/{buildFiles.Length}[/]";
+                ctx.Status($"{progressRatio} - [bold]Building[/] {buildFile.FullName}");
 
-				runningProcess = Process.Start(new ProcessStartInfo("dotnet", "build /graphBuild")
-				{
-					WorkingDirectory = Path.GetDirectoryName(buildFile.FullName),
-					UseShellExecute = false,
-					RedirectStandardOutput = true,
-					RedirectStandardError = true,
-				});
+                runningProcess = Process.Start(new ProcessStartInfo("dotnet", "build /graphBuild")
+                {
+                    WorkingDirectory = Path.GetDirectoryName(buildFile.FullName),
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                });
 
-				// equivalent of WaitforExit
-				var _output = await runningProcess.StandardOutput.ReadToEndAsync();
-				await runningProcess.WaitForExitAsync();
+                // equivalent of WaitforExit
+                var _output = await runningProcess.StandardOutput.ReadToEndAsync();
+                await runningProcess.WaitForExitAsync();
 
-				if (runningProcess.ExitCode == 0)
-				{
-					AnsiConsole.MarkupLine($"{progressRatio} - [green]completed[/] [bold]Building[/] [silver]{buildFile.Name}[/]");
-				}
-				else
-				{
-					AnsiConsole.MarkupLine($"{progressRatio} - [red]failed [bold]Building[/] {buildFile.Name} Exit Code: {runningProcess.ExitCode}[/]");
-					AnsiConsole.MarkupLine($"[grey]{_output}[/]");
-				}
+                if (runningProcess.ExitCode == 0)
+                {
+                    AnsiConsole.MarkupLine($"{progressRatio} - [green]completed[/] [bold]Building[/] [silver]{buildFile.Name}[/]");
+                }
+                else
+                {
+                    AnsiConsole.MarkupLine($"{progressRatio} - [red]failed [bold]Building[/] {buildFile.Name} Exit Code: {runningProcess.ExitCode}[/]");
+                    AnsiConsole.MarkupLine($"[grey]{_output}[/]");
+                }
 
-				if (cancellationToken.IsCancellationRequested)
-				{
-					break;
-				}
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
 
-				runningProcess.Kill(entireProcessTree: true);
-			}
-		});
+                runningProcess.Kill(entireProcessTree: true);
+            }
+        });
 
-		cancellationToken.Register(KillRunningProcesses);
-	}
+        cancellationToken.Register(KillRunningProcesses);
+    }
 
-	protected void KillRunningProcesses()
-	{
-		runningProcess.Kill(entireProcessTree: true);
+    protected void KillRunningProcesses()
+    {
+        runningProcess.Kill(entireProcessTree: true);
 
-		runningProcess.WaitForExit();
-	}
+        runningProcess.WaitForExit();
+    }
 }
