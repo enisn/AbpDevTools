5b935e75d9211dda6af9dbd420ab893a6675bf23
enisn <info@enisnecipoglu.com>
2026-07-06 14:17:35 +0300

    Skip git directories when discovering files

--- src/AbpDevTools/FileExplorer.cs
+++ src/AbpDevTools/FileExplorer.cs
@@ -3,15 +3,39 @@
 [RegisterTransient]
 public class FileExplorer
 {
+    private static readonly string[] DefaultExcludedFolders = { ".git" };
+
     public IEnumerable<string> FindDescendants(string path, string pattern)
     {
-        return Directory.EnumerateFiles(path, pattern, SearchOption.AllDirectories);
+        return FindDescendants(path, pattern, Array.Empty<string>());
     }
 
     public IEnumerable<string> FindDescendants(string path, string pattern, string[] excludeFolders)
     {
+        var excludedFolders = DefaultExcludedFolders
+            .Concat(excludeFolders ?? Array.Empty<string>())
+            .Distinct(StringComparer.OrdinalIgnoreCase)
+            .ToArray();
+
         return Directory.EnumerateFiles(path, pattern, SearchOption.AllDirectories)
-            .Where(x => !excludeFolders.Any(x.Contains));
+            .Where(x => !IsInExcludedFolder(path, x, excludedFolders));
+    }
+
+    private static bool IsInExcludedFolder(string rootPath, string filePath, string[] excludedFolders)
+    {
+        var relativePath = Path.GetRelativePath(rootPath, filePath);
+        var directoryPath = Path.GetDirectoryName(relativePath);
+
+        if (string.IsNullOrEmpty(directoryPath))
+        {
+            return false;
+        }
+
+        var directoryNames = directoryPath.Split(
+            new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        return directoryNames.Any(directoryName => excludedFolders.Contains(directoryName, StringComparer.OrdinalIgnoreCase));
     }
 
     public IEnumerable<string> FindAscendants(string path, string pattern)
