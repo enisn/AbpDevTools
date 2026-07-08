1dee752ccbca94b44426b5a7c2635df5f845d345
Enis Necipoglu <info@enisnecipoglu.com>
2026-07-08 13:44:24 +0300

    Use configured tools for self-update

--- tests/AbpDevTools.Tests/Commands/UpdateCheckCommandTests.cs
+++ tests/AbpDevTools.Tests/Commands/UpdateCheckCommandTests.cs
@@ -10,7 +10,7 @@ public class UpdateCheckCommandTests
     [Fact]
     public void CreateUpdateProcessStartInfo_Should_Not_Use_Shell_Execute()
     {
-        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345);
+        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345, "custom-pwsh", "custom-dotnet");
 
         startInfo.UseShellExecute.Should().BeFalse();
     }
@@ -18,23 +18,34 @@ public class UpdateCheckCommandTests
     [Fact]
     public void CreateUpdateProcessStartInfo_Should_Wait_For_Current_Process_Before_Update()
     {
-        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345);
+        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345, "custom-pwsh", "custom-dotnet");
+        var arguments = GetArguments(startInfo);
 
-        startInfo.Arguments.Should().Contain("12345");
-        startInfo.Arguments.Should().Contain("dotnet tool update -g AbpDevTools");
+        arguments.Should().Contain("12345");
+        arguments.Should().Contain("custom-dotnet");
+        arguments.Should().Contain("tool update -g AbpDevTools");
     }
 
     [Fact]
-    public void CreateUpdateProcessStartInfo_On_Windows_Should_Start_PowerShell_Directly()
+    public void CreateUpdateProcessStartInfo_On_Windows_Should_Use_Configured_PowerShell()
     {
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             return;
         }
 
-        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345);
+        var startInfo = UpdateCheckCommand.CreateUpdateProcessStartInfo(12345, "custom-pwsh", "custom-dotnet");
+        var arguments = GetArguments(startInfo);
 
-        startInfo.FileName.Should().Be("powershell.exe");
-        startInfo.Arguments.Should().Contain("Wait-Process -Id 12345");
+        startInfo.FileName.Should().Be("custom-pwsh");
+        arguments.Should().Contain("Wait-Process -Id 12345");
+        arguments.Should().NotContain("ExecutionPolicy");
+    }
+
+    private static string GetArguments(System.Diagnostics.ProcessStartInfo startInfo)
+    {
+        return startInfo.ArgumentList.Count > 0
+            ? string.Join(" ", startInfo.ArgumentList)
+            : startInfo.Arguments;
     }
 }
